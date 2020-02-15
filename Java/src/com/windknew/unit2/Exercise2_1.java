20234a617f8284dc62ed965f9dfe1101bcd1221f
pangyuanni <1396902579@qq.com>
2020-02-15 13:16:54 +0800

    在家学习java代码，Scanner

---
+++ Java/src/com/windknew/unit2/Exercise2_1.java
@@ -0,0 +1,15 @@
+package com.windknew.unit2;
+
+import java.util.Scanner;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pls inouut she shi wen du:");
+        double she = sc.nextDouble();
+        double hua = she*(9.0/5)+32;
+        System.out.println(hua);
+
+    }
+
+}
--- unit1/src/com/windknew/unit2/Exercise2_1.java
+++
@@ -1,4 +0,0 @@
-package com.windknew.unit2;
-
-public class Exercise2_1 {
-}
